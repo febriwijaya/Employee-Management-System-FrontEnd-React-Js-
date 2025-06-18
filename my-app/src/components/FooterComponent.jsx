e0b1630fa44c641c65095288bfe205ab112764d8
febri wijaya <59653659+febriwijaya@users.noreply.github.com>
2025-06-18 08:20:46 +0700

    First Commit FE
    First Commit Front End

---
+++ my-app/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className="footer container-fluid">
+        <span>All rights reserverd 2025 by asdosIT</span>
+      </footer>
+    </div>
+  );
+};
+
+export default FooterComponent;
---
+++ my-app/src/components/HeaderComponent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HeaderComponent = () => {
+  return (
+    <header>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a href="" className="navbar-brand">
+            Employee Management System
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default HeaderComponent;
